98dcb96a5a9bda506d87f9aa3c102465b60b6f3a
Vaibhav Singh Thapli <vaibhavthapli123@gmail.com>
2019-03-02 09:59:48 +0530

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,6 +1,8 @@
+#Tic-tac-toe game
+#!python3
+
 theBoard = {'top-L':'','top-M':'','top-R':'','mid-L':'','mid-M':'','mid-R':'','low-L':'','low-M':'','low-R':''}
 def printBoard(board):
-    print(board['top-L']+ ' | '+board['top-M'] + '|'+board['top-R'])
     print('+ + +')
     print(board['mid-L']+ ' | '+board['mid-M'] + '|'+board['mid-R'])
     print('+ + +')
